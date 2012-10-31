44f3b8dfcdbfc96df4e13e36d7b8b31bf6a6e17e
Matthew Pontefract <matthew@rethought-solutions.com>
2012-10-31 10:53:31 +0000

    setup record details enhanced; pep8 cleaned

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup, find_packages
-import os
 
 setup(
-    name = "rfclient",
-    version = "0.2.1",
-    
-    packages = find_packages('.'),
-    package_dir = {'':'.'},
+    name="rfclient",
+    version="0.2.1",
+    author="ReThought Ltd",
+    license="BSD",
+    keywords="api",
+    description="API to the RedFlash notification management system.",
+
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    packages=find_packages('.'),
+    package_dir={'': '.'},
 )
